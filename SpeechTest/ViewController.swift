71d53c0a8a527483064c242d3f2fac2a8b2cb196
Jacob <Jfmetcalf5@gmail.com>
2019-07-17 08:16:39 -0600

    Initial Commit

---
+++ SpeechTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpeechTest
+//
+//  Created by Jacob Metcalf on 7/17/19.
+//  Copyright © 2019 Jacob Metcalf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
